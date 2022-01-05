e56c091b0d3a292beb0b323c83dcc6e6ed66eadd
B-Sallum <brusallum@hotmail.com>
2022-01-05 05:27:24 -0300

    REAAAAAAAALLY CLEAN

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Hello universe</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
